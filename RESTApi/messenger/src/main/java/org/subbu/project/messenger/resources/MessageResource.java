11adf64775de7ba3cbbebc815794c78d025f9082
Subramanya <Subramanya@LAPTOP-BL4ENSRQ>
2018-02-07 20:55:10 -0600

    Path Param

--- RESTApi/messenger/src/main/java/org/subbu/project/messenger/resources/MessageResource.java
+++ RESTApi/messenger/src/main/java/org/subbu/project/messenger/resources/MessageResource.java
@@ -1,9 +1,15 @@
 package org.subbu.project.messenger.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.subbu.project.messenger.model.Message;
+import org.subbu.project.messenger.service.MessageService;
 
 @Path("/messages")
 public class MessageResource {
@@ -12,9 +18,18 @@ public class MessageResource {
 	public MessageResource() {
 		// TODO Auto-generated constructor stub
 	}
+MessageService messageService = new MessageService();
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages(){
-		return "Hello";
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages() {
+		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id) {	
+		return messageService.getMessage(id);
 	}
 }
